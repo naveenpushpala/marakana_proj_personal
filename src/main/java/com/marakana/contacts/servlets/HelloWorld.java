2bc20cf37538fcc4e908f18ad658db1b3b46cc4a
krishna <krishna@krishna-PC>
2017-04-11 11:54:02 +0530

    2012-09-24 14:01
    commiting views integration with servlets.

---
+++ src/main/java/com/marakana/contacts/servlets/HelloWorld.java
@@ -0,0 +1,32 @@
+package com.marakana.contacts.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorld
+ */
+@WebServlet("/hello-servlet")
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	/*	response.setContentType("text/html");
+		response.getWriter().println("<html><body><h1>HelloWorld</h1></body></html>");
+	*/
+		RequestDispatcher view = request.getRequestDispatcher("jsp/hello.jsp");
+		view.forward(request, response);
+	}
+
+
+}
